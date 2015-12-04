79f0b9dbd9af2e859ebeb3aef624ed56ffb9942d
Rathie <vifaaralexander@null.net>
2015-12-04 14:27:13 +0000

    Added

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,244 @@
+#include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+#include <cstdlib>
+#include <ctime>
+#include "SimpleAudioEngine.h"  
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+Label* labelTouchInfo;
+Sprite* Player;
+Sprite* Planet;
+Sprite* Stars[50];
+Sprite* spaceObjects[10];
+ParticleSystemQuad* spaceShipThruster;
+float movementSpeed;
+float spaceObjectSpeed;
+float starSpeed;
+Vec2 pos;
+Vec2 playerPos;
+Vec2 temp;
+bool hasBegun;
+int screenSizeY;
+int screenSizeX;
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+	srand(time(NULL));
+
+
+    auto rootNode = CSLoader::createNode("MainScene.csb");
+
+    addChild(rootNode);
+
+	this->scheduleUpdate();
+	cocos2d::Size frameSize = cocos2d::Director::getInstance()->getOpenGLView()->getFrameSize();
+	screenSizeY = frameSize.height;
+	screenSizeX = frameSize.width;
+	Device::setAccelerometerEnabled(true);
+
+
+
+
+	auto touchListener = EventListenerTouchOneByOne::create();
+
+	touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+	touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
+	touchListener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
+	touchListener->onTouchCancelled = CC_CALLBACK_2(HelloWorld::onTouchCancelled, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+	cocos2d::Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
+
+	auto keyboardListener = EventListenerKeyboard::create();
+	keyboardListener->onKeyPressed = CC_CALLBACK_2(HelloWorld::onKeyPressed, this);
+	keyboardListener->onKeyReleased = CC_CALLBACK_2(HelloWorld::onKeyReleased, this);
+
+	auto AccelListener = EventListenerAcceleration::create(CC_CALLBACK_2(HelloWorld::onAcceleration, this));
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(AccelListener, this);
+
+
+	Player = (Sprite*)rootNode->getChildByName("Player");
+	Planet = (Sprite*)rootNode->getChildByName("Planet");
+	spaceShipThruster = (ParticleSystemQuad*)rootNode->getChildByName("ShipThrusters");
+	for (int i = 0; i < 10; i++)
+	{
+		if (i % 2)
+		{
+			spaceObjects[i] = Sprite::create("Object1.png");
+		}
+		else
+		{
+			spaceObjects[i] = Sprite::create("Object2.png");
+		}
+		this->addChild(spaceObjects[i]);
+		spaceObjects[i]->setPositionX(rand() % screenSizeX);
+		spaceObjects[i]->setPositionY(rand() % screenSizeY);
+	}
+	this->schedule(schedule_selector(HelloWorld::UpdateTimer), 1.0f);
+
+	
+	for (int i = 0; i < 50; i++)
+	{
+		if (i % 2)
+		{
+			Stars[i] = Sprite::create("Untitled2.png");
+		}
+		else
+		{
+			Stars[i] = Sprite::create("Untitled.png");
+		}
+		this->addChild(Stars[i]);
+		Stars[i]->setPositionX(rand() % screenSizeX);
+		Stars[i]->setPositionY(rand() % screenSizeY);
+	}
+	this->addChild(Player);
+	this->addChild(Planet);
+	this->addChild(spaceShipThruster);
+
+	hasBegun = false;
+	movementSpeed = 2.125f;
+	spaceObjectSpeed = 0.125f;
+	starSpeed = 1.0f;
+
+	CocosDenshion::SimpleAudioEngine::sharedEngine()->playBackgroundMusic(
+		"Enter Shikari - The Jester (Pro).WAV", true);
+    return true;
+}
+void HelloWorld::UpdateTimer(float dt)
+{
+	if(spaceObjectSpeed < 85)
+	spaceObjectSpeed += 0.225f;
+	if(starSpeed < 100)
+	starSpeed += 0.3f;
+}
+void HelloWorld::update(float delta)
+{
+	if (hasBegun == true)
+	{
+		playerPos = Player->getPosition();
+		if (pos.x >= playerPos.x)
+		{
+			playerPos.x += movementSpeed;
+		}
+		if (pos.x <= playerPos.x)
+		{
+			playerPos.x -= movementSpeed;
+		}
+		if (pos.y >= playerPos.y)
+		{
+			playerPos.y += movementSpeed;
+		}
+		if (pos.y <= playerPos.y)
+		{
+			playerPos.y -= movementSpeed;
+		}
+		spaceShipThruster->setPositionX(Player->getPositionX());
+		spaceShipThruster->setPositionY(Player->getPositionY() - 30);
+		Player->setPosition(playerPos);
+		temp = Planet->getPosition();
+		temp.y -= movementSpeed;
+		Planet->setPosition(temp);
+		
+		for (int i = 0; i < 50; i++)
+		{
+			
+			Vec2 starPos = Stars[i]->getPosition();
+			starPos.y -= starSpeed;
+			Stars[i]->setPosition(starPos);
+			if (Stars[i]->getPositionY() <= 0 || Stars[i]->getPositionY() > screenSizeY)
+			{
+				Stars[i]->setPositionY(screenSizeY);
+				Stars[i]->setPositionX(rand() % screenSizeX);
+
+			}
+
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			Vec2 spaceObjectPos = spaceObjects[i]->getPosition();
+			spaceObjectPos.y -= spaceObjectSpeed;
+			spaceObjects[i]->setPosition(spaceObjectPos);
+			if (spaceObjects[i]->getPositionY() <= 0 || spaceObjects[i]->getPositionY() > screenSizeY)
+			{
+				spaceObjects[i]->setPositionY(screenSizeY);
+				spaceObjects[i]->setPositionX(rand() % screenSizeX);
+
+			}
+
+		}
+		if (Player->getPositionX() > screenSizeX)
+		{
+			Player->setPositionX(0);
+		}
+		if (Player->getPositionX() < 0)
+		{
+			Player->setPositionX(screenSizeX);
+		}
+		if (Player->getPositionY() > screenSizeY)
+		{
+			Player->setPositionY(0);
+		}
+		if (Player->getPositionY() < 0)
+		{
+			Player->setPositionY(screenSizeY);
+		}
+	}
+}
+
+bool HelloWorld::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+	hasBegun = true;
+	pos = touch->getLocation();
+	
+	return true;
+
+}
+
+void HelloWorld::onAcceleration(cocos2d::Acceleration* accel, cocos2d::Event* event)
+{
+	
+	pos.x += accel->y;
+	pos.y += accel->x;
+}
+
+void HelloWorld::onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+
+}
+
+void HelloWorld::onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+	pos = touch->getLocation();
+
+}
+
+void HelloWorld::onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+
+}
---
+++ cocosstudio/MainScene.csd
@@ -0,0 +1,68 @@
+<GameProjectFile>
+  <PropertyGroup Type="Scene" Name="MainScene" ID="a2ee0952-26b5-49ae-8bf9-4f1d6279b798" Version="2.3.2.3" />
+  <Content ctype="GameProjectContent">
+    <Content>
+      <Animation Duration="0" Speed="1.0000" />
+      <ObjectData Name="Scene" ctype="GameNodeObjectData">
+        <Size X="640.0000" Y="960.0000" />
+        <Children>
+          <AbstractNodeData Name="Default" ActionTag="-620272433" Tag="5" RotationSkewX="90.0000" RotationSkewY="90.0000" IconVisible="False" LeftMargin="-161.1853" RightMargin="-158.8147" TopMargin="159.9989" BottomMargin="160.0011" ctype="SpriteObjectData">
+            <Size X="960.0000" Y="640.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="318.8147" Y="480.0011" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.4981" Y="0.5000" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <FileData Type="Normal" Path="1329766522.png.png" Plist="" />
+            <BlendFunc Src="1" Dst="771" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="Player" ActionTag="1046708977" Tag="4" IconVisible="False" HorizontalEdge="BothEdge" VerticalEdge="BothEdge" LeftMargin="295.8593" RightMargin="292.1407" TopMargin="715.9284" BottomMargin="192.0716" ctype="SpriteObjectData">
+            <Size X="52.0000" Y="52.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.4528" />
+            <Position X="321.8560" Y="215.6160" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.5029" Y="0.2246" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <FileData Type="Normal" Path="shuttle2.png" Plist="" />
+            <BlendFunc Src="770" Dst="771" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="Planet" ActionTag="964634113" Tag="5" IconVisible="False" HorizontalEdge="BothEdge" VerticalEdge="BothEdge" LeftMargin="114.8259" RightMargin="493.1741" TopMargin="194.7969" BottomMargin="733.2031" ctype="SpriteObjectData">
+            <Size X="32.0000" Y="32.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="130.8160" Y="749.1840" />
+            <Scale ScaleX="1.6099" ScaleY="1.6099" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.2044" Y="0.7804" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <FileData Type="Normal" Path="Earth.png" Plist="" />
+            <BlendFunc Src="770" Dst="771" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="ShipThrusters" ActionTag="-876814774" Tag="5" IconVisible="True" LeftMargin="374.0000" RightMargin="266.0000" TopMargin="480.0000" BottomMargin="480.0000" ctype="ParticleObjectData">
+            <Size X="0.0000" Y="0.0000" />
+            <AnchorPoint />
+            <Position X="374.0000" Y="480.0000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.5844" Y="0.5000" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <FileData Type="Default" Path="Default/defaultParticle.plist" Plist="" />
+            <BlendFunc Src="775" Dst="1" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="SpaceObject" ActionTag="1573086580" Tag="6" IconVisible="False" LeftMargin="-6.0000" RightMargin="600.0000" TopMargin="425.0000" BottomMargin="489.0000" ctype="SpriteObjectData">
+            <Size X="49.0000" Y="49.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="17.0000" Y="512.0000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.0266" Y="0.5333" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <FileData Type="Normal" Path="Object1.png" Plist="" />
+            <BlendFunc Src="770" Dst="771" />
+          </AbstractNodeData>
+        </Children>
+      </ObjectData>
+    </Content>
+  </Content>
+</GameProjectFile>
